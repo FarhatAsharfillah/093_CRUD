f4e1ea2b261e0d8adad24d818d834b72a244fe4a
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-20 09:56:37 +0700

    menambahkan route utama ('/') untuk pengujian server, menambahkan endpoint GET /api/users untuk mengambil seluruh data dari tabel 'mahasiswa' dan menambahkan penanganan error saat koneksi dan query database

--- index.js
+++ index.js
@@ -21,3 +21,24 @@ const db = myspl.createConnection ({
     port : 3309
 })
 
+db.connect (err => {
+    if (err) {
+        console.error ('Gagal koneksi ke database'+err.stack);
+        return;
+    }
+    console.log ('Berhasil koneksi ke database');
+});
+
+//Buat Method GET dan POST 
+
+//GET
+app.get('api/users', (req, res) => {
+    db.query ('SELECT * FROM mahasiswa', (err, results) => {
+        if (err) {
+            console.error ('Gagal mengambil data'+ err.stack);
+            res.status(500).send ('Gagal mengambil data');
+            return;
+        }
+        res.json (results);
+    });
+});
