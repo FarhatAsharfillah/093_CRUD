c947007f7b50ce40734744ec84ba031963e55e3f
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-20 10:12:07 +0700

    feat(api): menambahkan PUT untuk update data mahasiswa
    Menambahkan route PUT untuk menambahkan data baru ke tabel 'mahasiswa',
    melakukan validasi input (nama, nim, dan kelas wajib diisi),
    menggunakan query parameterized untuk mencegah SQL injection,
    dan menambahkan respons sukses (201) dan pesan error yang sesuai

--- index.js
+++ index.js
@@ -62,3 +62,21 @@ app.post('/api/users', (req, res) => {
         }
     );
 });
+
+//PUT
+//PUT
+app.put('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    const { nama, nim, kelas } = req.body;
+    db.query (
+        'UPDATE mahasiswa SET nama = ?, nim = ?, kelas = ? WHERE id = ?',
+        [nama, nim, kelas, userId],
+        (err, results) => {
+            if (err) {
+                console.error(err);
+                return res.status(500).json({ message: 'Gagal memperbarui data' });
+            }
+            res.json ({ message: 'Data berhasil diperbarui'});
+        }
+    );
+});
