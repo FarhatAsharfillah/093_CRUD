74a79e8a14619ee5c17ac7a0034aa2321a9955a1
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-20 09:55:46 +0700

    menyiapkan server Express dengan koneksi MySQL dan route GET dasar
    inisialisasi aplikasi Express dengan middleware JSON dan URL-encoded dan membuat koneksi ke database 'mahasiswa' menggunakan library mysql

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+let myspl = require('mysql');
+const app = express();
+const PORT = 3000;
+app.use (express.json());
+app.use (express.urlencoded({extended:true}));
+
+app.get('/', (req, res) => {
+    res.send ('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log (`Server is running on port ${PORT}`);
+});
+
+const db = myspl.createConnection ({
+    host :'localhost',
+    user : 'root',
+    password : 'Asharf251003',
+    database : 'mahasiswa',
+    port : 3309
+})
+
