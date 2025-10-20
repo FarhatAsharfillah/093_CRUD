c5d484208e6f4cfe711c7b3e077db72c33f26161
FarhatAsharfillah <farhatasharfillah03@gmail.com>
2025-10-20 10:15:38 +0700

    menambahkan endpoint DELETE /api/users/:id untuk menghapus data mahasiswa
    Menambahkan route DELETE untuk menghapus data mahasiswa berdasarkan ID,
    menggunakan parameter URL (:id) untuk menentukan data yang akan dihapus,
    menangani kemungkinan error saat proses penghapusan data di database, dan
    mengembalikan respons JSON berisi pesan sukses atau gagal

--- index.js
+++ index.js
@@ -80,3 +80,16 @@ app.put('/api/users/:id', (req, res) => {
         }
     );
 });
+
+//DELETE
+app.delete('/api/users/:id', (req, res) => {
+    const userId = req.params.id;
+    db.query ('DELETE FROM mahasiswa WHERE id = ?', [userId], (err, results) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).json({ message: 'Gagal menghapus data' });
+        }
+        res.json ({ message: 'Data berhasil dihapus'});
+        }
+    );
+});
